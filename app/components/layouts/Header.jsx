04a6e36d9217e7d053f68637e8492803a93588b9
rrathbone <rachellerathbone@gmail.com>
2016-11-22 22:36:21 -0800

    styled nav and added hero image

--- app/components/layouts/Header.jsx
+++ app/components/layouts/Header.jsx
@@ -4,18 +4,21 @@ import React from 'react';
 const Header = React.createClass({
   render() {
     return (
-      <nav>
-        <div id="logo">
-          <Link to='/'>Refined</Link>
+    <header>
+      <div className="twelve columns">
+        <div id="logo" className="six columns">
+          <Link to='/'><img src="images/logo_white.png"/></Link>
         </div>
-
-        <ul>
-          <li><Link to='/login'>Login</Link></li>
-          <li><Link to='/products'>Products</Link></li>
-          <li><Link to='/guides'>Guides</Link></li>
-          <li><Link to='/cart'>Cart</Link></li>
-        </ul>
-      </nav>
+        <nav className="six columns">
+          <ul>
+            <li><Link to='/login'>Login</Link></li>
+            <li><Link to='/products'>Products</Link></li>
+            <li><Link to='/guides'>Guides</Link></li>
+            <li><Link to='/cart'>Cart</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
     )
   }
 });
