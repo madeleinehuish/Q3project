7bb2d0f8e05f1a91cde12bace87c6d0cb6ae2d46
rrathbone <rachellerathbone@gmail.com>
2016-11-22 18:48:06 -0800

    add products to seed file, change description from string to text, and added skeleton cdn

--- migrations/20161121154531_products.js
+++ migrations/20161121154531_products.js
@@ -7,7 +7,7 @@ exports.up = function(knex) {
     table.string('image').notNullable().defaultTo('');
     table.enu('rating', [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]).notNullable();
     table.decimal('price', 8, 2);
-    table.string('description').notNullable().defaultTo('');
+    table.text('description').notNullable().defaultTo('');
     table.string('keywords').notNullable().defaultTo('');
     table.boolean('on_sale');
     table.timestamps(true, true);
