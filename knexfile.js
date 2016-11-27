e3a111b00625364392dc695ce83d182a7ef83d8c
rrathbone <rachellerathbone@gmail.com>
2016-11-26 16:20:49 -0800

    add production to kexfile

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/refined_dev'
+  },
+
+  "production": {
+    "client": "pg",
+    "connection": "process.env.DATABASE_URL"
   }
 };
