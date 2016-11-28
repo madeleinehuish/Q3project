450826d687d826d4c3a41c8bbdc964384e09825d
madeleinehuish <vasistha@hotmail.com>
2016-11-28 08:10:51 -0800

    signup working

--- routes/api-users.js
+++ routes/api-users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt-as-promised');
 const boom = require('boom');
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const knex = require('knex');
+const knex = require('../knex');
 const { camelizeKeys, decamelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
